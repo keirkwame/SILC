7c0e11fc27f4711244bbe3d26a84cc2255decc45
Keir Rogers <kwame@Keirs-MacBook-Pro.local>
2016-01-04 19:12:30 +0000

    Updated plotting code for old biased maps

--- python/diffuse_mask.py
+++ python/diffuse_mask.py
@@ -4,12 +4,14 @@ import math as mh
 import copy as cp
 
 if __name__ == "__main__":
-    niter = 2000
-    old_map = hp.read_map('/Users/keir/Documents/s2let_ilc_planck/maps/PR2/frequencyMaps/HFI_SkyMap_857_2048_R2.00_full.fits')
-    mask = hp.read_map('/Users/keir/Documents/s2let_ilc_planck/nilc_pr1_builtmask.fits') #0 where holes
+    niter = 20000
+    old_map = hp.read_map('/Users/kwame/Documents/s2let_ilc_data/s2let_ilc_covar15_planck_diffuse_deconv_tapered_thresh_lmax3600_3600_hybridC_6_1_recon_inpaint800.fits')
+    mask = np.load('/Users/kwame/Documents/s2let_ilc_data/nilc_pr1_builtmask_holes_ring_800.npy') #0 where holes
     new_map = cp.deepcopy(old_map)
-    nside = hp.get_nside(mask)
-    hole_pixs = np.where(mask == 0)[0]
+    #nside = hp.get_nside(mask)
+    #hole_pixs = np.where(mask == 0)[0]
+    nside = 2048
+    hole_pixs = mask[0]
     '''theta = mh.pi / 2.
     phi = 0.
     rad_arcmin = 21
@@ -23,4 +25,4 @@ if __name__ == "__main__":
 
     resid_map = new_map - old_map
 
-    hp.write_map('/Users/keir/Documents/s2let_ilc_planck/diffuse_data/planck_diffuse_857_pr2.fits',new_map)
+    hp.write_map('/Users/kwame/Documents/s2let_ilc_data/s2let_ilc_covar15_planck_diffuse_deconv_tapered_thresh_lmax3600_3600_hybridC_6_1_recon_inpaint800GD20000.fits',new_map)
